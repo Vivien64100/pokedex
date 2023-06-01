eff7d3c11f77f838afd402a91e7edde9de9f2f5d
Vivien Floureck <vivienfloureck@outlook.com>
2023-06-01 10:08:41 +0200

    add quest 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+  
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <div>
-      <PokemonCard/>
+      < PokemonCard pokemon={pokemonList[1]}
+      />
      </div>
     </>
   )
 }
 
 export default App
+
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,14 @@
 
-const PokemonCard = () =>{
+const PokemonCard = ({pokemon}) =>{
 
-    const pokemon = pokemonList[1];
+console.log(pokemon);
+
     return ( 
     <figure>
          { pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt= {pokemon.name} />) : (<p>???</p>)}
          <figcaption>{pokemon.name}</figcaption>
     </figure>     
- );
-}
+ ); ;
+} 
 
-export default PokemonCard
-
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+export default PokemonCard;
