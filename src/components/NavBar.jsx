2664f520b0b05239e927522dfc2d4489e8af275b
Vivien Floureck <vivienfloureck@outlook.com>
2023-06-07 12:06:51 +0200

    add .map on NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 import PropTypes from "prop-types";
+import PokemonCard from './PokemonCard';
 
 
 const NavBar = ({pokeprops,pokemonList}) => {
   const [pokemonCount , setPokemonCount] = pokeprops
 
-  const handleClickSui = () => 
-  setPokemonCount(
-    pokemonCount < pokemonList.length -1 ? pokemonCount +1 : pokemonCount);
+ 
 
-  const handleClickPre = () => 
-  setPokemonCount (
-    pokemonCount > 0 ? pokemonCount - 1 : pokemonCount );
+  const handleClick = (pokemonCount) => {
+    setPokemonCount(pokemonCount);
+  };
+
     
     return (
-        <div>
-        {pokemonCount > 0 ? (
-            <button onClick={handleClickPre}>Précédent</button>
-          ) : undefined}
-          {pokemonCount < pokemonList.length -1 ? (
-            <button onClick={handleClickSui}>Suivant </button>
-          ) : undefined}
-        </div>
+      <div>
+      {pokemonList.map((pokemon, pokemonCount) => {
+         return (
+          <button key={pokemonCount} onClick={() =>handleClick(pokemonCount)}>{pokemon.name}
+         </button>);
+         })}
+
+ </div>
     )
 }
 
